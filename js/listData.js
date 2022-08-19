7b5692c39f6bd5e6a46a3c809551534cdb3bfd83
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-19 13:41:54 +0530

    Added: Validations check

---
+++ js/listData.js
@@ -0,0 +1,83 @@
+class listData {
+    get id(){
+        return this._id
+    }
+    set id(id) {
+        this._id = id
+    }
+
+    get vegetable(){
+        return this._vegetable;
+    }
+    set vegetable(vegetable) {
+
+        let vegRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if (vegRegex.test(vegetable))
+            this._vegetable = vegetable;
+        else
+            throw new Error("Incorrect Vegetable Name");     
+    }
+
+    get fruits(){
+        return this._fruits;
+    }
+    set fruits(fruits) {
+
+        let fruitsRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if (fruitsRegex.test(fruits))
+            this._fruits = fruits;
+        else
+            throw new Error("Incorrect Fruit Name");    
+    }
+
+    get others(){
+        return this._others;
+    }
+    set others(others) {
+
+        let othersRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if (othersRegex.test(others))
+            this._others = others;
+        else
+            throw new Error("Incorrect Name"); 
+    }
+
+    get vegetableQuantity(){
+        return this._vegetableQuantity;
+    }
+    set vegetableQuantity(vegetableQuantity) {
+        let vegQRegex = RegExp('^[a-zA-Z0-9\\s]{2,}$');
+        if (vegQRegex.test(vegetableQuantity))
+            this._vegetableQuantity = vegetableQuantity;
+        else
+            throw new Error("Enter vegetable quantity"); 
+    }
+
+    get fruitsQuantity(){
+        return this._fruitsQuantity;
+    }
+    set fruitsQuantity(fruitsQuantity) {
+        let fruitsQRegex = RegExp('^[a-zA-Z0-9\\s]{2,}$');
+        if (fruitsQRegex.test(fruitsQuantity))       
+            this._fruitsQuantity = fruitsQuantity;
+        else
+            throw new Error("Enter Fruit Quantity"); 
+    }
+
+    get othersQuantity(){
+        return this._othersQuantity;
+    }
+    set othersQuantity(othersQuantity) {
+        let othersQRegex = RegExp('^[a-zA-Z0-9\\s]{2,}$');
+        if (othersQRegex.test(othersQuantity))       
+            this._othersQuantity = othersQuantity;
+        else
+            throw new Error("Enter Quantity"); 
+    }
+
+    toString() {
+        return "id=" + this.id + "vegetable = " + this.vegetable + ", vegetablQuantity = " + this.vegetableQuantity + ", fruits = " + this.fruits + ", fruitsQuantity = " + this.fruitsQuantity + ", Others = " + this.others + 
+          ", othersQuantity = " + this.othersQuantity;
+    }
+
+}
---
+++ js/listHome.js
@@ -0,0 +1,98 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    validateName();
+    validateQuantity();
+
+});
+
+
+function validateName() {
+    const vegetable = document.querySelector('#vegetable');
+    const vegError = document.querySelector('.veg-error');
+    vegetable.addEventListener('input', function () {
+        if (vegetable.value.length == 0) {
+            vegError.textContent = "";
+            return;
+        }
+        try {
+            (new listData()).vegetable = vegetable.value;
+            vegError.textContent = "";
+        } catch (e) {
+            vegError.textContent = e;
+        }
+    });
+    const fruits = document.querySelector('#fruits');
+    const frError = document.querySelector('.fruits-error');
+    fruits.addEventListener('input', function () {
+        if (fruits.value.length == 0) {
+            frError.textContent = "";
+            return;
+        }
+        try {
+            (new listData()).fruits = fruits.value;
+            frError.textContent = "";
+        } catch (e) {
+            frError.textContent = e;
+        }
+    });
+    const others = document.querySelector('#others');
+    const otherError = document.querySelector('.others-error');
+    others.addEventListener('input', function () {
+        if (others.value.length == 0) {
+            otherError.textContent = "";
+            return;
+        }
+        try {
+            (new listData()).others = others.value;
+            otherError.textContent = "";
+        } catch (e) {
+            otherError.textContent = e;
+        }
+    });
+}
+
+function validateQuantity() {
+    const vegetableQ = document.querySelector('#vegetableQuantity');
+    const vegError = document.querySelector('.vegQ-error');
+    vegetableQ.addEventListener('input', function () {
+        if (vegetableQ.value.length == 0) {
+            vegError.textContent = "";
+            return;
+        }
+        try {
+            (new listData()).vegetableQuantity = vegetableQuantity.value;
+            vegError.textContent = "";
+        } catch (e) {
+            vegError.textContent = e;
+        }
+    });
+    const fruitsQ = document.querySelector('#fruitsQuantity');
+    const frError = document.querySelector('.fruitsQ-error');
+    fruitsQ.addEventListener('input', function () {
+        if (fruitsQ.value.length == 0) {
+            frError.textContent = "";
+            return;
+        }
+        try {
+            (new listData()).fruitsQuantity = fruitsQuantity.value;
+            frError.textContent = "";
+        } catch (e) {
+            frError.textContent = e;
+        }
+    });
+
+    const othersQ = document.querySelector('#othersQuantity');
+    const otherError = document.querySelector('.othersQ-error');
+    othersQ.addEventListener('input', function () {
+        if (othersQ.value.length == 0) {
+            otherError.textContent = "";
+            return;
+        }
+        try {
+            (new listData()).othersQuantity = othersQuantity.value;
+            otherError.textContent = "";
+        } catch (e) {
+            otherError.textContent = e;
+        }
+    });
+
+}
